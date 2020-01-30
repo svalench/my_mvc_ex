7f68c360c7d19f24f0ac219d8869d2da3a2bab45
svalench <chitsalex@gmail.com>
2020-01-30 13:51:16 -0800

    add twig

--- controller/my.php
+++ controller/my.php
@@ -1,4 +1,12 @@
 <?php
+require_once $_SERVER["DOCUMENT_ROOT"].'/vendor/autoload.php';
 print_r("this my.php file!!!!");
 print_r($_GET);
+
+$loader = new \Twig\Loader\ArrayLoader([
+    'index' => 'Hello {{ name }}!',
+]);
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index', ['name' => 'Fabien']);
 ?>
