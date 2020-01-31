81b30897e83a23e1bf6f54f6fb7f14102203cc8f
svalench <chitsalex@gmail.com>
2020-01-31 13:01:00 -0800

    add point to insert, add file with parametrs

---
+++ core/parametrs.php
@@ -0,0 +1,14 @@
+<?php
+// create app name
+define("APP_NAME", "kultyapa");
+
+//create connection
+$connection = [
+"type" 		=> "mysql",
+"nameDB" 	=> "",
+"host" 		=> "localhost",
+"user"		=> "",
+"pass"		=> ""
+];
+
+?>
--- route/route.php
+++ route/route.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 
 include_once $_SERVER["DOCUMENT_ROOT"].'/route/index.php';
 
